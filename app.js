086c9560bf4cc460d25e2c0452deacde451239ba
Laylalll <yusioulai@gmail.com>
2022-02-23 11:05:31 +0800

    modify /search code

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    const name = restaurant.name.toLowerCase()
+    const category = restaurant.category.toLowerCase()
+    return name.includes(keyword.toLowerCase()) || category.includes(keyword.toLowerCase())
+  })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
